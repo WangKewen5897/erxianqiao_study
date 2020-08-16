d8ff197261487c00457a6a4b2bfa045ddf7c678b
wkw <m18584065897@163.com>
2020-08-16 10:38:06 +0800

    改版24点游戏

--- src/com/wkw/GameOf24/GameOf24.java
+++ src/com/wkw/GameOf24/GameOf24.java
@@ -3,42 +3,45 @@ package com.wkw.GameOf24;
 import java.util.Scanner;
 
 public class GameOf24 {
-    private void operate(int n){
-        if(n<1||n>100000){
-            System.out.print("数值越界");
-        }
-        if(n<4){
-            System.out.println("NO");
-        }else if(n%2==0){
-            System.out.println("YES");
-            System.out.println("2*4=8");
-            System.out.println("3*8=24");
-            System.out.println("1*24=24");
-            while(n>4){
-                System.out.println(n+"-"+(n-1)+"=1");
-                System.out.println("1*24=24");
-                n-=2;
-            }
-        }else if(n%2==1){
-            System.out.println("YES");
-            System.out.println("5-2=1");
-            System.out.println("1+3=4");
-            System.out.println("4+4=8");
-            System.out.println("8*3=24");
-            while(n>5){
-                System.out.println(n+"-"+(n-1)+"=1");
-                System.out.println("1*24=24");
-                n-=2;
-            }
-        }
-    }
+//    StringBuilder str1 = new StringBuilder("YES\n"+"2*4=8\n"+"3*8=24\n"+"1*24=24\n"); //str1追加固定部分的公式
+//    StringBuilder str2 = new StringBuilder("YES\n"+"5-2=3\n"+"1+3=4\n"+"4+4=8\n"+"3*8=24\n");
+//    private void operate(int n){
+//
+//    }
     public static void main(String[] args) {
-            System.out.println("请输入n的数值：");
+        StringBuilder str1 = new StringBuilder("YES\n"+"2 * 4 = 8\n"+"3 * 8 = 24\n"+"1 * 24 = 24\n"); //str1追加固定部分的公式
+        StringBuilder str2 = new StringBuilder("YES\n"+"5 - 2 = 3\n"+"1 + 3 = 4\n"+"4 + 4 = 8\n"+"3 * 8 = 24\n");
+//            System.out.println("请输入n的数值：");
             Scanner in = new Scanner(System.in);
             int n = in.nextInt();
-            GameOf24 op = new GameOf24();
-            op.operate(n);
+        if(n%2==0){
+            if(n>=4&&n<=100000){
+                while(n>4){
+                    String s = n+" - "+(n-1)+" = 1\n";
+                    str1.append(s);
+                    str1.append("1 * 24 = 24\n");
+                    n-=2;
+                }
+                System.out.println(str1);
+            } else {
+                System.out.println("NO");
+            }
+        }else if(n%2==1){
+            if(n>=5&&n<=100000){
+                while(n>5){
+                    String s1 = n+" - "+(n-1)+" = 1\n";
+                    str2.append(s1);
+                    str2.append("1 * 24 = 24\n");
+                    n-=2;
+                }
+                System.out.println(str2);
+            }else {
+                System.out.println("NO");
+            }
+        }else {
+            System.out.println("NO");
         }
-
-
+//            GameOf24 op = new GameOf24();
+//            op.operate(n);
+        }
 }
